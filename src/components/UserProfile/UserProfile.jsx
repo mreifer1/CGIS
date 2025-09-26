80668a5ec34e6b424af0b9ed9641c69ef2fcb4f1
Michael Reifer <Mjreifer1@gmail.com>
2025-09-26 19:47:34 -0400

    just need to get button underneath picture in desktop mode

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,4 +1,4 @@
-// import './UserProfile.css'
+import './UserProfile.css'
 import axios from 'axios';
 import FetchButton from '../FetchButton';
 import {useState, useEffect} from 'react';
@@ -21,35 +21,37 @@ const UserProfile = () => {
         console.log(data)
     }
 
+    const dobFormater = (dob) => {
+        return `${dob.slice(5,7)}-${dob.slice(8,10)}-${dob.slice(0,4)}`
+    }
+
     if (!userInfo.results) {
         return <div>Loading...</div>;
     }
 
     return (
-      
        <div className="container">
-            <div className="img">
+            <div className="imgContainer">
                 <img src={userInfo.results[0].picture.large} />
             </div>
-            <div className="info" style={{color: 'white'}}>
-                <ul>
-                    <li>{`${userInfo.results[0].name.first} ${userInfo.results[0].name.last}`}</li>
-                    <li>
-                        {
-                        `${userInfo.results[0].location.street.number} 
-                        ${userInfo.results[0].location.street.name} 
-                        ${userInfo.results[0].location.city}
-                        ${userInfo.results[0].location.state}
-                        ${userInfo.results[0].location.postcode}
-                        `
-                        }
-                    </li>
-                    <li>{userInfo.results[0].email}</li>
-                    <li>{userInfo.results[0].cell}</li>
-                    <li>{userInfo.results[0].dob.date.slice(0,10).split('-').reverse().join('-')}</li>
-                </ul>
+                <div className="infoContainer">
+                    <ul className="userInfo">
+                        <li>{`${userInfo.results[0].name.first} ${userInfo.results[0].name.last}`}</li>
+                        <li>
+                            {
+                            `${userInfo.results[0].location.street.number},
+                            ${userInfo.results[0].location.street.name},
+                            ${userInfo.results[0].location.city},
+                            ${userInfo.results[0].location.state},
+                            ${userInfo.results[0].location.postcode}
+                            `
+                            }
+                        </li>
+                        <li>{userInfo.results[0].email}</li>
+                        <li>{userInfo.results[0].phone}</li>
+                        <li>{dobFormater(userInfo.results[0].dob.date)}</li>
+                    </ul>
             </div>
-
             <FetchButton userData={newUserData}/>
        </div>
     )
