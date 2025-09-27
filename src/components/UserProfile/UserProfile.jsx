66f81e2e2790a322426bcfe20b5d330a6f29db80
Michael Reifer <Mjreifer1@gmail.com>
2025-09-27 07:30:51 -0400

    task complete

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,61 +1,63 @@
-import './UserProfile.css'
+import './UserProfile.css';
 import axios from 'axios';
-import FetchButton from '../FetchButton';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 const UserProfile = () => {
-    const [userInfo, setUserInfo] = useState({});
+  const [userInfo, setUserInfo] = useState({});
 
-    useEffect(() => {
-        axios.get("https://randomuser.me/api")
-        .then((res) => {
-            setUserInfo(res.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-    }, [])
+  const fetchUser = () => {
+    axios.get("https://randomuser.me/api")
+      .then((res) => {
+        setUserInfo(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const newUserData = (data) => {
-        setUserInfo(data)
-        console.log(data)
-    }
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
-    const dobFormater = (dob) => {
-        return `${dob.slice(5,7)}-${dob.slice(8,10)}-${dob.slice(0,4)}`
-    }
+  const dobFormater = (dob) => {
+    return `${dob.slice(5, 7)}-${dob.slice(8, 10)}-${dob.slice(0, 4)}`;
+  };
 
-    if (!userInfo.results) {
-        return <div>Loading...</div>;
-    }
+  if (!userInfo.results) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-       <div className="container">
-            <div className="imgContainer">
-                <img src={userInfo.results[0].picture.large} />
-            </div>
-                <div className="infoContainer">
-                    <ul className="userInfo">
-                        <li>{`${userInfo.results[0].name.first} ${userInfo.results[0].name.last}`}</li>
-                        <li>
-                            {
-                            `${userInfo.results[0].location.street.number},
-                            ${userInfo.results[0].location.street.name},
-                            ${userInfo.results[0].location.city},
-                            ${userInfo.results[0].location.state},
-                            ${userInfo.results[0].location.postcode}
-                            `
-                            }
-                        </li>
-                        <li>{userInfo.results[0].email}</li>
-                        <li>{userInfo.results[0].phone}</li>
-                        <li>{dobFormater(userInfo.results[0].dob.date)}</li>
-                    </ul>
-            </div>
-            <FetchButton userData={newUserData}/>
-       </div>
-    )
-}
+  return (
+    <div className="container">
+      <div className="leftColumn">
+        <div className="imgContainer">
+          <img src={userInfo.results[0].picture.large} alt="User" />
+        </div>
+        <div className="buttonContainer desktopOnly">
+          <button onClick={fetchUser}>Fetch New User</button>
+        </div>
+      </div>
 
+      <div className="rightColumn">
+        <ul className="userInfo">
+          <li>{`${userInfo.results[0].name.first} ${userInfo.results[0].name.last}`}</li>
+          <li>
+            {`${userInfo.results[0].location.street.number},
+            ${userInfo.results[0].location.street.name},
+            ${userInfo.results[0].location.city},
+            ${userInfo.results[0].location.state},
+            ${userInfo.results[0].location.postcode}`}
+          </li>
+          <li>{userInfo.results[0].email}</li>
+          <li>{userInfo.results[0].phone}</li>
+          <li>{dobFormater(userInfo.results[0].dob.date)}</li>
+        </ul>
+        <div className="buttonContainer mobileOnly">
+          <button onClick={fetchUser}>Fetch New User</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default UserProfile;
